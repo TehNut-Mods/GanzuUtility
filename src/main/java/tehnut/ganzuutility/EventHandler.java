ff0438a9fa69b0b7d1e14104eb13c5208c0cc228
TehNut <nick_ignoffo@yahoo.com>
2015-02-21 12:52:38 -0800

    Why did I think that was a good idea?

--- src/main/java/tehnut/ganzuutility/EventHandler.java
+++ src/main/java/tehnut/ganzuutility/EventHandler.java
@@ -3,7 +3,6 @@ package tehnut.ganzuutility;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.block.BlockLeaves;
 import net.minecraft.init.Items;
-import net.minecraft.item.ItemShears;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.world.BlockEvent;
 
@@ -14,8 +13,8 @@ public class EventHandler {
 	Random random = new Random();
 
 	@SubscribeEvent
-	public void onBlockBreak(BlockEvent.HarvestDropsEvent event) {
-		if (event.block instanceof BlockLeaves && !event.isSilkTouching && event.harvester.getHeldItem().getItem() instanceof ItemShears)
+	public void onHarvestDrops(BlockEvent.HarvestDropsEvent event) {
+		if (event.block instanceof BlockLeaves && !event.isSilkTouching)
 			if (random.nextInt(100) <= ConfigHandler.chance)
 				event.drops.add(new ItemStack(Items.stick, 1, 0));
 	}
